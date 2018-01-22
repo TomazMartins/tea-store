e55d5dcdd5e0026b34356e569bb77e84b5611000
TomazMartins <tomaz.r.martins@gmail.com>
2018-01-22 08:59:43 -0200

    Reestrutura os parâmetros da requisição
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- teastore/app/requesters/tea_requester.rb
+++ teastore/app/requesters/tea_requester.rb
@@ -1,18 +1,38 @@
-require 'uri'
 require 'net/http'
+require 'ostruct'
+require 'uri'
 
 class TeaRequester
-  BASE_URI = 'https://pokeapi.co/api/v2'
+  BASE_URI = 'https://tea-store.herokuapp.com/api'
 
   def request_teas
-    uri = URI.parse( "#{BASE_URI}/pokemon/1/" )
+    uri = URI.parse( "#{BASE_URI}/available-teas" )
 
-    http = Net::HTTP.new( uri.host, uri.port )
-    http.use_ssl = true
+    response = request( uri )
 
-    request = Net::HTTP::Get.new( uri )
-    response = http.request( request )
-
-    response.body
+    convert_tea_from( response )
   end
+
+  private
+    def convert_teas_from( json )
+      json_object = JSON.parse( json, object_class: OpenStruct )
+
+      teas = []
+
+      json_object.each do |object|
+        teas << Tea.new( category: object.category, price: object.price, is_menu: true )
+      end
+
+      teas
+    end
+
+    def request( uri )
+      http = Net::HTTP.new( uri.host, uri.port )
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new( uri )
+      response = http.request( request )
+
+      response.body
+    end
 end
--- teastore/lib/tasks/scheduled_tasks.rake
+++ teastore/lib/tasks/scheduled_tasks.rake
@@ -1,8 +1,10 @@
 namespace :scheduled_taks do
   task request_teas: :environment do
     tea_requester = TeaRequester.new
-    response = tea_requester.request_teas
+    teas = tea_requester.request_teas
 
-    Tea.create( category: response, price: 10.0, is_menu: true )
+    teas.each do |tea|
+      tea.save
+    end
   end
 end
